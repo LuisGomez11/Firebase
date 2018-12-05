f3160c4d516806362a017fc5baf000fdd590f74c
LuisGomez11 <luisgomez24g@gmail.com>
2018-12-04 20:55:42 -0500

    primer commit

---
+++ PrimeraPrueba/app.js
@@ -0,0 +1,101 @@
+function registrar() {
+    var email = document.getElementById('email').value;
+    var contraseña = document.getElementById('contraseña').value;
+
+    firebase.auth().createUserWithEmailAndPassword(email, contraseña)
+        .then(function () {
+            verificar();
+        })
+        .catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log(errorCode);
+            console.log(errorMessage);
+            // ...
+        });
+}
+
+function ingreso() {
+
+    var email2 = document.getElementById('email2').value;
+    var contraseña2 = document.getElementById('contraseña2').value;
+
+    firebase.auth().signInWithEmailAndPassword(email2, contraseña2)
+        .catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log(errorCode);
+            console.log(errorMessage);
+            // ...
+        });
+
+}
+
+function observador() {
+
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            // User is signed in.
+            console.log("Existe usurio activo");
+            aparece(user);
+            var displayName = user.displayName;
+            var email = user.email;
+
+            console.log("***************");
+            console.log(user.emailVerified);
+            console.log("***************");
+
+            var emailVerified = user.emailVerified;
+            var photoURL = user.photoURL;
+            var isAnonymous = user.isAnonymous;
+            var uid = user.uid;
+            var providerData = user.providerData;
+            // ...
+        } else {
+            // User is signed out.
+            console.log("No existe usurio activo");
+            contenido.innerHTML='';
+            // ...
+        }
+    });
+
+
+}
+
+observador();
+
+function aparece(user) {
+
+    var user = user;
+    var contenido = document.getElementById('contenido');
+    if (user.emailVerified) {
+        contenido.innerHTML = '<div><div class="alert alert-success" role="alert"><h4 class="alert-heading">Bienvenido! ${user.email} </h4> <p>Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.</p><hr><p class="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p></div><button onclick="cerrar()" class="btn btn-danger">Cerrar sesion </button></div>';
+    }
+    
+}
+
+function cerrar() {
+    firebase.auth().signOut()
+        .then(function () {
+            console.log("Saliendo...");
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+}
+
+function verificar() {
+
+    var user = firebase.auth().currentUser;
+
+    user.sendEmailVerification().then(function () {
+        // Email sent.
+        console.log("Enviando correo, verifique");
+    }).catch(function (error) {
+        // An error happened.
+        console.log(error);
+    });
+
+}
